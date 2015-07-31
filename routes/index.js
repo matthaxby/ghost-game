9c5a1868389f3a653aa88428ee0c3d51fe8ba91f
Matt Haxby <matthaxby@gmail.com>
2015-07-30 21:38:01 -0600

    desaturated player profile, so ghosty!

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,12 @@ var router = express.Router()
 var game = require('../lib/checker')
 var db = require('monk')(process.env.MONGOLAB_URI)
 var games = db.get('games')
-var players = db.get('players')
-
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { user: req.user })
 });
-
-
-
-router.get('/games/:id', function(req, res, next) {
-  if (req.isAuthenticated()) {
-    console.log(req.user.id)
-    games.findOne({player: req.user.id}, function(err, doc) {
-      if (!doc) {
-        console.log('a')
-        games.insert({player: req.user.id})
-        res.redirect('/games/' + req.user.id)
-      } else {
-        console.log('b')
-        console.log(doc)
-        res.render('games', {user: req.user, games: doc})
-      }
-    })
-  } else {
-    res.redirect('/')
-  }
-})
 
 router.post('/play', function(req, res, next) {
   games.findOne({player: req.user.id}, function (err, doc) {
@@ -58,23 +35,14 @@ router.post('/play', function(req, res, next) {
 router.get('/play', function(req, res, next) {
   games.findOne({player: req.user.id}, function(err, doc) {
     if (!doc) {
-      console.log('a')
-      games.insert({player: req.user.id})
+      games.insert({player: req.user.id, word: ''})
     }
-    res.render('play', {user: req.user, games: doc})
+    res.rendxer('play', {user: req.user, games: doc})
   })
 })
-
-router.post('/games/:id', function (req, res, next) {
-  games.update({player: req.user.id}, {player: req.user.id, games: {word: 'buc'}})
-  res.redirect('/games/' + req.user.id)
-})
-
 
 router.get('/instructions', function(req, res, next) {
   res.render('instructions', {user: req.user})
 })
-
-
 
 module.exports = router
